4c205b747d872f46f046a3664959112b2d737d34
AadityaAgarwal <73215428+AadityaAgarwal@users.noreply.github.com>
2021-12-31 15:58:38 +0530

    Update Ball.js

--- Components/Ball.js
+++ Components/Ball.js
@@ -18,11 +18,11 @@ AFRAME.registerComponent("ball", {
   
           var camera = document.querySelector("#camera").object3D;
   
-          //get the camera direction as Three.js Vector
+
           var direction = new THREE.Vector3();
           camera.getWorldDirection(direction);
   
-          //set the velocity and it's direction
+
           ball.setAttribute("velocity", direction.multiplyScalar(-10));
           ball.setAttribute('position',{y:0.5})
   
